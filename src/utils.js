a5736a497018b86c0ab2822743e87a788eeb7fff
ahertzog <alon.hertzog@hexagon.com>
2025-08-17 01:09:03 +0300

    switch

--- src/utils.js
+++
@@ -1,9 +0,0 @@
-export const createPageUrl = (pageName) => {
-    const paramsIndex = pageName.indexOf('?');
-    if (paramsIndex !== -1) {
-        const page = pageName.substring(0, paramsIndex);
-        const params = pageName.substring(paramsIndex);
-        return `/${page}${params}`;
-    }
-    return `/${pageName}`;
-};
